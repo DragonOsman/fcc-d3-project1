7623359b13dc54df8f43cce43abf0a7c0613d10a
Osman Zakir <osmanzakir90@hotmail.com>
2020-11-28 02:56:04 +0500

    fixed indent problem after recompile

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,26 @@
 "use strict";
 fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json", {
-    method: "GET",
-    mode: "cors"
+  method: "GET",
+  mode: "cors"
 })
-    .then(response => {
+  .then(response => {
     if (response.ok) {
-        return response.json();
+      return response.json();
     }
-})
-    .then(data => {
+  })
+  .then(data => {
     const svgWidth = 13000;
     const svgHeight = 500;
     const padding = 50;
     const container = d3.select("#bar-chart")
-        .append("svg")
-        .attr("width", svgWidth)
-        .attr("height", svgHeight)
-        .attr("x", 0)
-        .attr("y", 0);
+      .append("svg")
+      .attr("width", svgWidth)
+      .attr("height", svgHeight)
+      .attr("x", 0)
+      .attr("y", 0);
     const dates = [];
     for (const elem of data.data) {
-        dates.push(`${new Date(elem[0]).getMonth() + 1}-${new Date(elem[0]).getDate()}-${new Date(elem[0]).getFullYear()}`);
+      dates.push(`${new Date(elem[0]).getMonth() + 1}-${new Date(elem[0]).getDate()}-${new Date(elem[0]).getFullYear()}`);
     }
     const xScale = d3.scaleBand();
     xScale.domain(dates);
@@ -33,12 +33,12 @@ fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
     const xAxis = d3.axisBottom(xScale);
     const yAxis = d3.axisLeft(yScale);
     container.append("g")
-        .attr("transform", `translate(0, ${svgHeight - padding})`)
-        .attr("id", "x-axis")
-        .call(xAxis);
+      .attr("transform", `translate(0, ${svgHeight - padding})`)
+      .attr("id", "x-axis")
+      .call(xAxis);
     container.append("g")
-        .attr("transform", `translate(${padding}, 0)`)
-        .attr("id", "y-axis")
-        .call(yAxis);
-})
-    .catch(error => console.log(error));
+      .attr("transform", `translate(${padding}, 0)`)
+      .attr("id", "y-axis")
+      .call(yAxis);
+  })
+  .catch(error => console.log(error));
