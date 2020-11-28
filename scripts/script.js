a3ea8acc6d75db406a5efb073441021e418a7180
Osman Zakir <osmanzakir90@hotmail.com>
2020-11-28 16:22:08 +0500

    recompiled JS code

--- scripts/script.js
+++ scripts/script.js
@@ -1,44 +1,43 @@
 "use strict";
 fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json", {
-  method: "GET",
-  mode: "cors"
+    method: "GET",
+    mode: "cors"
 })
-  .then(response => {
+    .then(response => {
     if (response.ok) {
-      return response.json();
+        return response.json();
     }
-  })
-  .then(data => {
-    const svgWidth = 13000;
+})
+    .then(data => {
+    const dates = [];
+    for (const elem of data.data) {
+        dates.push(`${new Date(elem[0]).getMonth() + 1}-${new Date(elem[0]).getDate()}-${new Date(elem[0]).getFullYear()}`);
+    }
+    const svgWidth = (data.data.length * dates[0].length) * 10;
+    console.log(`svgWidth: ${svgWidth}`);
     const svgHeight = 500;
     const padding = 50;
     const container = d3.select("#bar-chart")
-      .append("svg")
-      .attr("width", svgWidth)
-      .attr("height", svgHeight)
-      .attr("x", 0)
-      .attr("y", 0);
-    const dates = [];
-    for (const elem of data.data) {
-      dates.push(`${new Date(elem[0]).getMonth() + 1}-${new Date(elem[0]).getDate()}-${new Date(elem[0]).getFullYear()}`);
-    }
+        .append("svg")
+        .attr("width", svgWidth)
+        .attr("height", svgHeight)
+        .attr("x", 0)
+        .attr("y", 0);
     const xScale = d3.scaleBand();
     xScale.domain(dates);
-    console.log(xScale);
     xScale.range([padding, svgWidth - padding]);
     const yScale = d3.scaleLinear();
     yScale.domain([0, 19000]);
-    console.log(yScale);
     yScale.range([svgHeight - padding, padding]);
     const xAxis = d3.axisBottom(xScale);
     const yAxis = d3.axisLeft(yScale);
     container.append("g")
-      .attr("transform", `translate(0, ${svgHeight - padding})`)
-      .attr("id", "x-axis")
-      .call(xAxis);
+        .attr("transform", `translate(0, ${svgHeight - padding})`)
+        .attr("id", "x-axis")
+        .call(xAxis);
     container.append("g")
-      .attr("transform", `translate(${padding}, 0)`)
-      .attr("id", "y-axis")
-      .call(yAxis);
-  })
-  .catch(error => console.log(error));
+        .attr("transform", `translate(${padding}, 0)`)
+        .attr("id", "y-axis")
+        .call(yAxis);
+})
+    .catch(error => console.log(error));
