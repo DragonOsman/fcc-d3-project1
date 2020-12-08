968b68aba6d9cf9dcb304fd73d28833b019baaa9
Osman Zakir <osmanzakir90@hotmail.com>
2020-12-09 03:49:21 +0500

    faced a problem with data in d3 with typescript, so took out typescript and switched to plain js

--- scripts/script.js
+++ scripts/script.js
@@ -1,54 +1,68 @@
 "use strict";
+
 fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json", {
-    method: "GET",
-    mode: "cors"
+  method: "GET",
+  mode: "cors"
 })
-    .then(response => {
+  .then(response => {
     if (response.status >= 200 && response.status < 400) {
-        return response.json();
+      return response.json();
     }
-})
-    .then(data => {
-    const gdpData = [...data.data];
+  })
+  .then(data => {
     const dates = [];
-    for (const elem of gdpData) {
-        dates.push(`${new Date(elem[0]).getMonth() + 1}-${new Date(elem[0]).getDate()}-${new Date(elem[0]).getFullYear()}`);
+    for (const elem of data.data) {
+      dates.push(new Date(elem[0]));
     }
-    const svgWidth = (data.data.length * dates[0].length) * 10;
-    const svgHeight = 500;
-    const padding = 50;
+
+    const barWidth = 5;
+    const svgWidth = (barWidth * data.data.length) + dates[0].toString().length + 60;
+    const svgHeight = 600;
+    const padding = 40;
     const container = d3.select("#bar-chart")
-        .append("svg")
-        .attr("width", svgWidth)
-        .attr("height", svgHeight)
-        .attr("x", 0)
-        .attr("y", 0);
-    const xScale = d3.scaleBand();
-    xScale.domain(dates);
+      .append("svg")
+      .attr("width", svgWidth)
+      .attr("height", svgHeight)
+      .attr("x", 0)
+      .attr("y", 0)
+    ;
+
+    const xMax = d3.max(dates);
+    xMax.setMonth(xMax.getMonth() + 3);
+    const xScale = d3.scaleTime();
+    xScale.domain([d3.min(dates), xMax]);
     xScale.range([padding, svgWidth - padding]);
     const yScale = d3.scaleLinear();
-    yScale.domain([0, 19000]);
+    yScale.domain([0, data.data[data.data.length - 1][1]]);
     yScale.range([svgHeight - padding, padding]);
+
     const xAxis = d3.axisBottom(xScale);
     const yAxis = d3.axisLeft(yScale);
+
     container.selectAll("rect")
-        .data(gdpData)
-        .enter()
-        .append("rect")
-        .attr("data-date", d => d[0])
-        .attr("data-gdp", d => d[1])
-        .attr("x", (d, i) => i * 23)
-        .attr("y", d => svgHeight - d[1] - padding) // height of SVG canvas - bar height - padding
-        .attr("width", 20)
-        .attr("height", d => d[1])
-        .attr("class", "bar");
+      .data(data.data)
+      .enter()
+      .append("rect")
+      .attr("data-date", d => d[0])
+      .attr("data-gdp", d => d[1])
+      .attr("x", (d, i) => xScale(dates[i]))
+      .attr("y", d => (svgHeight - ((svgHeight - yScale(d[1])) - padding) - padding))
+      .attr("width", barWidth)
+      .attr("height", d => (svgHeight - yScale(d[1])) - padding)
+      .attr("class", "bar")
+    ;
+
     container.append("g")
-        .attr("transform", `translate(0, ${svgHeight - padding})`)
-        .attr("id", "x-axis")
-        .call(xAxis);
+      .attr("transform", `translate(0, ${svgHeight - padding})`)
+      .attr("id", "x-axis")
+      .call(xAxis)
+    ;
+
     container.append("g")
-        .attr("transform", `translate(${padding}, 0)`)
-        .attr("id", "y-axis")
-        .call(yAxis);
-})
-    .catch(error => console.log(error));
+      .attr("transform", `translate(${padding}, 0)`)
+      .attr("id", "y-axis")
+      .call(yAxis)
+    ;
+  })
+  .catch(error => console.log(error))
+;
