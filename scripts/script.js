14a99d8b0fb3110cb3dd80182b8def6b612406c6
Osman Zakir <osmanzakir90@hotmail.com>
2020-12-10 02:04:19 +0500

    fixed issue with space between last two bars

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,25 @@
 "use strict";
 
-fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json", {
-  method: "GET",
-  mode: "cors"
-})
-  .then(response => {
-    if (response.status >= 200 && response.status < 400) {
-      return response.json();
-    }
+const state = {
+  data: []
+};
+
+d3.json(
+  "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json", {
+    method: "GET",
+    mode: "cors"
   })
   .then(data => {
+    state.data = data.data;
     const dates = [];
-    for (const elem of data.data) {
+    for (const elem of state.data) {
       dates.push(new Date(elem[0]));
     }
 
     const barWidth = 5;
-    const svgWidth = (barWidth * data.data.length) + dates[0].toString().length + 60;
     const svgHeight = 600;
     const padding = 40;
+    const svgWidth = (barWidth * state.data.length) + dates[0].toString().length + 60;
     const container = d3.select("#bar-chart")
       .append("svg")
       .attr("width", svgWidth)
@@ -29,23 +30,28 @@ fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
 
     const xMax = d3.max(dates);
     xMax.setMonth(xMax.getMonth() + 3);
-    const xScale = d3.scaleTime();
-    xScale.domain([d3.min(dates), xMax]);
-    xScale.range([padding, svgWidth - padding]);
-    const yScale = d3.scaleLinear();
-    yScale.domain([0, data.data[data.data.length - 1][1]]);
-    yScale.range([svgHeight - padding, padding]);
+    const xScale = d3.scaleTime()
+      .domain([d3.min(dates), xMax])
+      .range([padding, svgWidth - padding]);
+    const yScale = d3.scaleLinear()
+      .domain([0, state.data[state.data.length - 1][1]])
+      .range([svgHeight - padding, padding]);
 
     const xAxis = d3.axisBottom(xScale);
     const yAxis = d3.axisLeft(yScale);
 
     container.selectAll("rect")
-      .data(data.data)
+      .data(state.data)
       .enter()
       .append("rect")
       .attr("data-date", d => d[0])
       .attr("data-gdp", d => d[1])
-      .attr("x", (d, i) => xScale(dates[i]))
+      .attr("x", (d, i) => {
+        if (i !== (state.data.length - 1)) {
+          return xScale(dates[i]);
+        }
+        return xScale(dates[i]) - barWidth;
+      })
       .attr("y", d => (svgHeight - ((svgHeight - yScale(d[1])) - padding) - padding))
       .attr("width", barWidth)
       .attr("height", d => (svgHeight - yScale(d[1])) - padding)
