5deec89e2571c6d266960dbe6cca5c6cfe66c19d
Osman Zakir <osmanzakir90@hotmail.com>
2020-12-12 19:39:18 +0500

    added position: absolute rule to tooltip and completed all code and user stories

--- scripts/script.js
+++ scripts/script.js
@@ -16,6 +16,10 @@ d3.json(
       dates.push(new Date(elem[0]));
     }
 
+    for (const date of dates) {
+      date.setMonth(date.getMonth() + 1);
+    }
+
     const barWidth = 5;
     const svgHeight = 600;
     const padding = 40;
@@ -40,6 +44,19 @@ d3.json(
     const xAxis = d3.axisBottom(xScale);
     const yAxis = d3.axisLeft(yScale);
 
+    const tooltip = d3.select(".tooltip-container")
+      .append("div")
+      .attr("id", "tooltip")
+      .attr("class", "tooltip")
+      .style("opacity", 0)
+    ;
+
+    const overlay = d3.select(".tooltip-container")
+      .append("div")
+      .attr("class", "overlay")
+      .style("opacity", 0)
+    ;
+
     container.selectAll("rect")
       .data(state.data)
       .enter()
@@ -56,6 +73,44 @@ d3.json(
       .attr("width", barWidth)
       .attr("height", d => (svgHeight - yScale(d[1])) - padding)
       .attr("class", "bar")
+      .on("mouseover", (e, d) => {
+        overlay.transition()
+          .duration(0)
+          .style("opacity", 0.9)
+          .attr("height", `${barWidth + 2}px`)
+          .attr("width", `${barWidth + 2}px`)
+          .style("left", `${e.pageX}px`)
+          .style("top", `${e.pageY - 28}px`)
+          .style("transform", "translateX(10px)")
+        ;
+
+        tooltip.transition()
+          .duration(200)
+          .style("opacity", 0.9)
+        ;
+         
+        tooltip.html(
+          `${d[0]}
+          <br />(format: yyyy-mm-dd)
+          <br />$${d[1].toFixed(1).replace(/(\d)(?=(\d{3})+\.)/g, "$1,")} Billion`
+        )
+          .attr("data-date", d[0])
+          .style("left", `${e.pageX}px`)
+          .style("top", `${e.pageY - 28}px`)
+          .style("transform", "translateX(10px)")
+        ;
+      })
+      .on("mouseout", () => {
+        tooltip.transition()
+          .duration(200)
+          .style("opacity", 0)
+        ;
+
+        overlay.transition()
+          .duration(200)
+          .style("opacity", 0)
+        ;
+      })
     ;
 
     container.append("g")
